2d84a3a925a033d790c2e1c52f649d8c309d81cb
Liz Moody <moodybam@gmail.com>
2024-05-11 09:43:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using static Product;
+
+public class Product
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Press 1 to add a product");
+        Console.WriteLine("Type 'exit' to quit");
+        string? userInput = Console.ReadLine();
+        userInput = string.Empty;
+        if (userInput == "1")
+        {
+            Console.WriteLine("Enter Product Name");
+            Product product;
+            userInput = Console.ReadLine();
+
+        }
+        else if (userInput == "2") 
+        {
+            Console.WriteLine("Enter name of the Dogleash");
+            var dogLeash = Console.ReadLine();
+            var dogLeash = productLogic.GetDogLeash(dogLeash);
+
+        }
+    }
+}
+
+
---
+++ set.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using static Product;
+
+namespace ScruffandScalesPetStore
+{
+    internal record Product(decimal Price, int Quantity, string Description, int LengthInches, string Material, string Name, double WeightPounds, bool IsKittenFood)
+    {
+
+    }
+}
